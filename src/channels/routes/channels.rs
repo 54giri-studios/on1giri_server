66523ab34cbf0c94ce6a0a8f01910465b6639f1d
Moogs <saphielle.akiyama@gmail.com>
2024-04-20 19:32:45 +0200

    Create channel route

--- src/channels/routes/channels.rs
+++ src/channels/routes/channels.rs
@@ -1,9 +1,9 @@
 use diesel::prelude::*;
 
-use diesel_async::RunQueryDsl;
-use rocket::{serde::json::Json, State};
+use diesel_async::{pooled_connection::deadpool::Pool, RunQueryDsl};
+use rocket::{serde::json::Json, Error, State};
 
-use crate::{Channel, DbPool, ErrorResponse};
+use crate::{Channel, DbPool, ErrorResponse, NewChannel};
 
 #[get("/<channel_id>")]
 pub async fn get_channel(pool: &State<DbPool>, channel_id: i32) -> Result<Json<Channel>, Json<ErrorResponse>> {
@@ -25,5 +25,28 @@ pub async fn get_channel(pool: &State<DbPool>, channel_id: i32) -> Result<Json<C
         .map_err(ErrorResponse::from)
         .map(|e| e.into())
         .map_err(|a| a.into())
+}
 
+#[post("/create", format = "json", data = "<new_channel>")]
+pub async fn post_channel<'a>(
+    pool: &State<DbPool>, 
+    new_channel: Json<NewChannel<'a>>
+) -> Result<Json<Channel<'a>>, Json<ErrorResponse>> {
+    let mut conn = match pool.get().await {
+        Ok(pool) => pool,
+        Err(err) => return Err(ErrorResponse::internal_error(err).into()),
+    };
+
+    use crate::schema::channels::dsl as c_dsl;
+
+    let maybe_channel: Result<Channel<'_>, _> = diesel::insert_into(c_dsl::channels)
+        .values(new_channel.into_inner())
+        .returning(Channel::as_returning())
+        .get_result(&mut conn)
+        .await;
+
+    match maybe_channel {
+        Ok(channel) => Ok(channel.into()),
+        Err(err) => Err(ErrorResponse::from(err).into())
+    }
 }
--- src/channels/routes/mod.rs
+++ src/channels/routes/mod.rs
@@ -7,7 +7,7 @@ pub fn routes() -> Vec<Route> {
     let mut routes = routes![];
 
     routes.extend(routes![messages::get_channel_history]);
-    routes.extend(routes![channels::get_channel]);
+    routes.extend(routes![channels::get_channel, channels::post_channel]);
 
     routes
 }
