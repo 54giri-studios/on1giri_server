234fd6c18e2f8aeda60621efce7d60019c2efa40
Moogs <saphielle.akiyama@gmail.com>
2024-04-21 16:04:47 +0200

    Fix get_user return type to UserMetadata

--- src/users/routes/get.rs
+++ src/users/routes/get.rs
@@ -4,31 +4,30 @@ use diesel::prelude::*;
 use diesel_async::RunQueryDsl;
 use rocket::{serde::json::Json, State};
 
-use crate::{DbPool, ErrorResponse, Guild, User};
+use crate::{DbPool, ErrorResponse, Guild, User, UserMetadata};
 
 /// Gets an user from its id
 /// 
 /// # Arguments
 /// * `id` - The user's unique identifier
 #[get("/<id>")]
-pub async fn get_by_id<'a>(pool: &State<DbPool>, id: i32) -> Json<User<'a>> {
-    use crate::schema::users::dsl::users;
+pub async fn get_by_id<'a>(pool: &State<DbPool>, id: i32) -> Result<Json<UserMetadata>, Json<ErrorResponse>> {
+    use crate::schema::users_metadata::dsl as um_dsl;
 
-    let mut conn = pool.get().await.unwrap();
+    let mut conn = match pool.get().await {
+        Ok(pool) => pool,
+        Err(err) => return Err(ErrorResponse::internal_error(err).into()),
+    };
 
-    let maybe_user: Result<User<'a>, _> = users
-        .select(User::as_select())
-        .find(id)
-        .first(&mut conn)
+    let maybe_user: Result<UserMetadata, _> = um_dsl::users_metadata
+        .select(UserMetadata::as_select())
+        .filter(um_dsl::id.eq(id))
+        .get_result(&mut conn)
         .await;
 
     match maybe_user {
-        Ok(user) => {
-            Json::from(user)
-        }
-        Err(e) => {
-            todo!();
-        }
+        Ok(user) => Ok(user.into()),
+        Err(err) => Err(ErrorResponse::from(err).into())
     }
 }
 
